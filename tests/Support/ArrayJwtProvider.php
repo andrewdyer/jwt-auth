6b5a7d141ead5c669784bba946d3b20860e39a1f
Andrew Dyer <andrewdyer@outlook.com>
2026-03-25 11:20:07 +0000

    feat: :sparkles: add JwtProviderInterface
    Added JwtProviderInterface and test ArrayJwtProvider implementation

---
+++ tests/Support/ArrayJwtProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AndrewDyer\JwtAuth\Tests\Support;
+
+use AndrewDyer\JwtAuth\Contracts\JwtProviderInterface;
+
+final class ArrayJwtProvider implements JwtProviderInterface
+{
+    public array $lastEncoded = [];
+
+    public function decode(string $token): mixed
+    {
+        return json_decode(base64_decode($token));
+    }
+
+    public function decodeUnverified(string $token): mixed
+    {
+        return json_decode(base64_decode($token));
+    }
+
+    public function encode(array $claims): string
+    {
+        $this->lastEncoded = $claims;
+
+        return base64_encode(json_encode($claims));
+    }
+}
