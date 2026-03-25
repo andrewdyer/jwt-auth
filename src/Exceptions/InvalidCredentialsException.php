f7cfa607ca9b1c4aa1f9ccf9281858c2ca7fbaa8
Andrew Dyer <andrewdyer@outlook.com>
2026-03-25 21:02:49 +0000

    fix: :bug: update exception constructors
    Refactor InvalidCredentialsException and InvalidTokenException to use constructor for message handling.

--- src/Exceptions/InvalidCredentialsException.php
+++ src/Exceptions/InvalidCredentialsException.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace AndrewDyer\JwtAuth\Exceptions;
 
 use RuntimeException;
+use Throwable;
 
 final class InvalidCredentialsException extends RuntimeException
 {
-    protected $message = 'Invalid credentials provided.';
+    public function __construct(string $message = 'Invalid credentials provided.', int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
--- src/Exceptions/InvalidTokenException.php
+++ src/Exceptions/InvalidTokenException.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace AndrewDyer\JwtAuth\Exceptions;
 
 use RuntimeException;
+use Throwable;
 
 final class InvalidTokenException extends RuntimeException
 {
-    protected $message = 'Invalid token provided.';
+    public function __construct(string $message = 'Invalid token provided.', int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
